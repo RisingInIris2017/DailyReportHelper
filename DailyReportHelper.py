ee1cf5ee3fdee5b0f3ac966d400f2bf3f799b015
Isaac Thompson <risinginiris2017@gmail.com>
2022-07-25 23:53:44 +0800

    1. 新增了补交昨天作业的功能 2. 现在如果不是补交作业，那么提交作业后程序会休眠一段时间以免大量重复交作业

--- DailyReportHelper.py
+++ DailyReportHelper.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from datetime import timedelta
 import yaml
 import itchat
 import pytz
@@ -7,6 +8,8 @@ from sys import exit
 import win32api
 import win32con
 from itchat.content import TEXT, ATTACHMENT
+from time import sleep
+from http.client import RemoteDisconnected
 
 # CONSTS
 # 配置文件的路径
@@ -14,10 +17,14 @@ CONFIG_PATH = abspath(r'.\config.yml')
 # 程序常量
 # 主动提交作业时，如果还没有写作业，那么发这张图作为提示
 PROMPT_MYSELF = abspath(r'.\prompt_myself.png')
+# 补交昨天的作业时，如果没有写昨天的作业，那么发这张图作为提示
+PROMPT_YESTERDAY = abspath(r'.\prompt_yesterday.png')
 # 其他同学提交作业时，如果还没有写作业，那么发这张图作为提示
 PROMPT_OTHERS = abspath(r'.\prompt_others.png')
 # 将微信“文件传输助手”的 ID 写成字面常量
 YOURSELF = "filehelper"
+# 一小时为 3600 秒
+HOURS = 3600.0
 
 
 # 专门用于发送提示的函数
@@ -65,7 +72,8 @@ itchat.auto_login(loginCallback=display_welcome, enableCmdQR=2, hotReload=True, 
 def get_target_group_username(msg, working_mode):
     # 主动交作业和群聊触发交作业这两种场景下，msg 字典的结构不同
     # 由 working_mode 参数控制，切换两种不同的工作逻辑来寻找指定的群聊信息
-    if working_mode == "myself":
+    # 主动交作业和补交作业的逻辑是一样的
+    if working_mode == "myself" or working_mode == "yesterday":
         search_result = itchat.search_chatrooms(name=TARGET_GROUP)
         if search_result is None:
             hint("未找到指定群聊，请检查程序错误", "每日汇报小助手提醒")
@@ -89,30 +97,59 @@ def get_target_group_username(msg, working_mode):
 
 
 def send_file(target_group_user_name, working_mode):
-    path = (DAILY_REPORT_FOLDER + '\\' +  # 文件夹路径
-            # 文件名规范：4位年2位月2位日-每日汇报.pdf
-            datetime.now(pytz.timezone('Asia/Shanghai')).strftime('%Y%m%d') + "-每日汇报.pdf")
+    if not working_mode == "yesterday":
+        path = (DAILY_REPORT_FOLDER + '\\' +  # 文件夹路径
+                # 文件名规范：4位年2位月2位日-每日汇报.pdf
+                datetime.now(pytz.timezone('Asia/Shanghai')).strftime('%Y%m%d') + "-每日汇报.pdf")
+    else:
+        yesterday = datetime.now(pytz.timezone('Asia/Shanghai')) - timedelta(days=1)
+        path = (DAILY_REPORT_FOLDER + '\\' +  # 文件夹路径
+                # 文件名规范：4位年2位月2位日-每日汇报.pdf
+                yesterday.strftime('%Y%m%d') + "-每日汇报.pdf")
     try:
         test = open(path)
         test.close()
         itchat.send('@fil@%s' % path, target_group_user_name)
         hint("已将文件发送到指定的群：%s" % TARGET_GROUP, "每日汇报小助手-发送成功")
+        # 如果不是补交作业的话，让小助手休息四个小时，此时正常来讲应该已经到第二天了
+        if not working_mode == "yesterday":
+            sleep(4 * HOURS)
     except FileNotFoundError:
-        image_path = PROMPT_MYSELF if (working_mode == "myself") else PROMPT_OTHERS
-        itchat.send('@img@%s' % image_path, YOURSELF)
+        if working_mode == "myself":
+            image_path = PROMPT_MYSELF
+            itchat.send('@img@%s' % image_path, YOURSELF)
+        elif working_mode == "yesterday":
+            image_path = PROMPT_YESTERDAY
+            itchat.send('@img@%s' % image_path, YOURSELF)
+        elif working_mode == "others":
+            image_path = PROMPT_OTHERS
+            itchat.send('@img@%s' % image_path, YOURSELF)
+        else:
+            hint("工作模式参数有误，无法发送提示图片", "每日汇报小助手提醒")
 
 
-# Work Logic
+# 工作逻辑
 # 手动触发：主动交作业
 @itchat.msg_register(TEXT)
-def upload_files_by_myself(msg):
+def upload_files_actively(msg):
     if msg["ToUserName"] == YOURSELF and msg["Text"] in [
         "交作业", "交日报", "交汇报", "交每日作业", "交每日日报", "交每日汇报",
         "发作业", "发日报", "发汇报", "发每日作业", "发每日日报", "发每日汇报"
     ]:
-        get_target_group = get_target_group_username(msg, "myself")
+        working_mode = "myself"
+        get_target_group = get_target_group_username(msg, working_mode)
         if get_target_group[0]:  # Flag 为真，表示拿到了目标群聊
-            send_file(get_target_group[1], "myself")
+            send_file(get_target_group[1], working_mode)
+        else:
+            hint("未找到目标群聊，请检查程序问题", "每日汇报小助手提醒")
+    elif msg["ToUserName"] == YOURSELF and msg["Text"] in [
+        "补交作业", "补交日报", "补交汇报", "补交每日作业", "补交每日日报", "补交每日汇报",
+        "补发作业", "补发日报", "补发汇报", "补发每日作业", "补发每日日报", "补发每日汇报"
+    ]:
+        working_mode = "yesterday"
+        get_target_group = get_target_group_username(msg, working_mode)
+        if get_target_group[0]:  # Flag 为真，表示拿到了目标群聊
+            send_file(get_target_group[1], working_mode)
         else:
             hint("未找到目标群聊，请检查程序问题", "每日汇报小助手提醒")
 
@@ -120,14 +157,15 @@ def upload_files_by_myself(msg):
 # 被动触发：晚上八点以后，如果教研室群有除了自己以外的人发送了附件，那么自动交作业
 @itchat.msg_register(ATTACHMENT, isGroupChat=True)
 def upload_files_when_prompted(msg):
+    working_mode = "others"
     current_time = [
         datetime.now(pytz.timezone('Asia/Shanghai')).hour,
         datetime.now(pytz.timezone('Asia/Shanghai')).minute
     ]
     if current_time[0] >= (time[0] + 1) or (current_time[1] >= time[1] and current_time[0] >= time[0]):
-        get_target_group = get_target_group_username(msg, "others")
+        get_target_group = get_target_group_username(msg, working_mode)
         if get_target_group[0]:
-            send_file(get_target_group[1], "others")
+            send_file(get_target_group[1], working_mode)
         else:
             hint("未找到目标群聊，请检查程序问题", "每日汇报小助手提醒")
     else:
